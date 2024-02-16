1557a2a1f209175d6c28b13f5bc8c9adebcc54fb
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2024-02-15 22:03:22 -0500

    Added dummy values for missing weeks

--- final_wildfire.py
+++ final_wildfire.py
@@ -46,16 +46,31 @@ def main():
     num_lat_splits = 12
 
     base_path = 'data/satellite-burn'
+    # For every year
     for year_folder in os.listdir(base_path):
+        # Make a 52 long array
+        weeks = [0] * 52
+        
         year_path = os.path.join(base_path, year_folder)
+        # Check if it is a directory and it is a year
         if not year_path.endswith('.csv') and year_folder.isdigit() and 2017 <= int(year_folder) <= 2023:
             if os.path.isdir(year_path):
+                # For every file in the year (every week)
                 for file in os.listdir(year_path):
                     file_path = os.path.join(year_path, file)
                     print(f"Reading file: {file_path}")
+                    print(f"File: {file}")
+                    week_num = int(file.split('_')[1].split('.')[0])
+                    weeks[week_num - 1] = 1
 
                     week_fire_df = get_week_fire_df(file_path, kelowna_stations_df, num_long_splits, num_lat_splits)
                     week_fire_df.to_csv(f'data/final-satellite-burn/{year_folder}/{file}', index=False, header=None)
+        
+        for i in range(52):
+            if weeks[i] == 0 and year_folder.isdigit() and 2017 <= int(year_folder) <= 2023:
+                print(f"Missing week: {i + 1}")
+                zero_df = pd.DataFrame(np.zeros(num_long_splits * num_lat_splits))
+                zero_df.to_csv(f'data/final-satellite-burn/{year_folder}/week_{i + 1}.csv', index=False, header=None)
 
 main()
         
