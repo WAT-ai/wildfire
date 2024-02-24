71cd488c0c40dac3d079b97cdbd007801e59552c
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2024-02-24 11:12:36 -0500

    Data ingestion v2

--- data_ingestion_script.py
+++ data_ingestion_script.py
@@ -3,11 +3,22 @@ import numpy as np
 import os
 
 
+def data_ingestion_v2():
+    print("Data Ingestion v2")
+    
+    for group in range(1, 24):
+        burn_path = 'data/training/quarterly-6-by-6/satellite-burn/group_' + str(group + 1) + '.csv'
+        weather_path = 'data/training/quarterly-6-by-6/weather/group_' + str(group) + '.csv'
+        burn_data_df = pd.read_csv(burn_path)
+        weather_data_df = pd.read_csv(weather_path)
+        print("Burn data for group " + str(group + 1) + ": "  + burn_data_df.head(36).to_string())
+        print("Weather data for group " + str(group) + ": " + weather_data_df.head(36).to_string())
+
 def data_ingestion():
     print("Data Ingestion")
     
     # Idea: keep n be weather data and n+1 be burn data
-    # Week 1: weather year x week 1, burn year x week 2
+    # Group 1: weather group 1, burn group 2
     # Week 52: weather year x week 52, burn year x+1 week 1
     
     for year in range(2017, 2023):
@@ -36,7 +47,7 @@ def data_ingestion():
                 break
 
 def main():
-    data_ingestion()
+    data_ingestion_v2()
 
 if __name__ == "__main__":
     main()
