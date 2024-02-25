77a151a234266a939d9b5ccc22f9773c207ff136
Ryan Chang <ryanchang0501@gmail.com>
2024-02-24 20:23:10 -0500

    init

--- xgboost_trainer.py
+++ xgboost_trainer.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import numpy as np
 import os
 from datetime import datetime
+import pickle
 
 def train(model, X, y, X_test, y_test, n_estimators, learning_rate, early_stopping_rounds):
     if(model == None):
@@ -28,6 +29,26 @@ def train(model, X, y, X_test, y_test, n_estimators, learning_rate, early_stoppi
 
     return model, mae, mse, rmse
 
+def get_training_data():
+    training_data = []
+    for group in range(1, 25):
+        weather_path = 'data/training/quarterly-6-by-6/weather/group_' + str(group) + '.csv'
+        burn_path = 'data/training/quarterly-6-by-6/satellite-burn/group_' + str(group + 1) + '.csv'
+
+        weather_data_df = pd.read_csv(weather_path)
+        burn_data = np.genfromtxt(burn_path, delimiter=',')
+
+        # print("Burn data for group " + str(group + 1) + ": "  + burn_data_df.head(36).to_string())
+        # print("Weather data for group " + str(group) + ": " + weather_data_df.head(36).to_string())
+
+        X = weather_data_df.values
+        y = burn_data
+
+        train_pair = (X, y)
+        training_data.append(train_pair)
+
+    return training_data
+
 def xgboost_trainer():
     model = None
     
@@ -35,28 +56,21 @@ def xgboost_trainer():
     mse_array = []
     rmse_array = []
 
-    # TODO: read in the final weather and satellite data
-    for i in range(0, 10):
-        X = pd.read_csv("./dummy-data/2016_weather_filtered.csv")
-        X = X.values
-        X
+    training_data = get_training_data()
 
-        y = np.genfromtxt("./dummy-data/2017_burn_filtered.csv", delimiter=',', skip_header=1)
-        y
+    for train_pair in training_data:
+        X = train_pair[0]
+        y = train_pair[1]
 
-        X_test = pd.read_csv("./dummy-data/2017_weather_filtered.csv")
-        X_test = X_test.values
-        X_test
-
-        y_test = np.genfromtxt("./dummy-data/2018_burn_filtered.csv", delimiter=',', skip_header=1)
-        y_test
+        X_test = X
+        y_test = y
 
         model, mae, mse, rmse = train(model, X, y, X_test, y_test, 1000, 0.01, 50)
         mae_array.append(mae)
         mse_array.append(mse)
         rmse_array.append(rmse)
 
-    save_results(mae_array, mse_array, rmse_array)
+    save_results(model, mae_array, mse_array, rmse_array)
 
 def get_run_notes():
     time_step = "time-step-"
@@ -65,12 +79,16 @@ def get_run_notes():
     fine_areas += input("Enter the number of fine areas: ")
     return time_step, fine_areas
 
-def save_results(mae_array, mse_array, rmse_array):
+def save_results(model, mae_array, mse_array, rmse_array):
     # Get information about this run 
     time_step, fine_areas = get_run_notes()
 
     # Get the current time
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Save model
+    file_name = f"trained-models/{current_time}_{time_step}_{fine_areas}.pkl"
+    pickle.dump(model, open(file_name, "wb"))
 
     # Create a folder with the current time as the name
     base_folder_name = "train-results"
