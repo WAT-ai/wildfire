d60672663d4e31b91cdbb0938958a17626ef14bf
Ryan Chang <ryanchang0501@gmail.com>
2024-03-01 20:51:29 -0500

    update file paths and color scale

--- predictions_data_vis.py
+++ predictions_data_vis.py
@@ -8,13 +8,23 @@ def get_color_for_value(value, cmap, min_value, max_value):
     norm = Normalize(vmin=min_value, vmax=max_value)
     return cmap(norm(value))
 
-def draw_grid(matrix, color_matrix, filename='output_plot.pdf'):  # Set the default filename
-    # Create a color map
+def create_color_map(color_matrix, cmap=cm.coolwarm):
     norm = Normalize(vmin=np.min(color_matrix), vmax=np.max(color_matrix))
     cmap = cm.coolwarm  # You can choose a different colormap if desired
     norm = Normalize(vmin=min(map(min, color_matrix)), vmax=max(map(max, color_matrix)))
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
+    return cmap, sm
+
+
+def draw_grid(matrix, error_matrix, color_matrix, filename='output_plot.pdf'):  # Set the default filename
+    # # Create a color map
+    # norm = Normalize(vmin=np.min(color_matrix), vmax=np.max(color_matrix))
+    # cmap = cm.coolwarm  # You can choose a different colormap if desired
+    # norm = Normalize(vmin=min(map(min, color_matrix)), vmax=max(map(max, color_matrix)))
+    # sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    # sm.set_array([])
+    cmap, sm = create_color_map(color_matrix)
 
     rows, cols = len(matrix), len(matrix[0])
 
@@ -34,8 +44,8 @@ def draw_grid(matrix, color_matrix, filename='output_plot.pdf'):  # Set the defa
             number = matrix[i][j]
             formatted_number = '{:.3f}'.format(number)[:6]  # Ensure at most 3 digits before the decimal point
             
-            color_value = color_matrix[i][j]
-            color = get_color_for_value(color_value, cmap, np.min(color_matrix), np.max(color_matrix))
+            error_value = error_matrix[i][j]
+            color = get_color_for_value(error_value, cmap, np.min(color_matrix), np.max(color_matrix))
 
             ax.add_patch(plt.Rectangle((j, i), 1, 1, fill=True, color=color))
             ax.text(j + 0.5, i + 0.5, formatted_number, ha='center', va='center', fontsize=12, color='white')
@@ -49,6 +59,8 @@ def draw_grid(matrix, color_matrix, filename='output_plot.pdf'):  # Set the defa
     # Add color bar
     cbar = plt.colorbar(sm, ax=ax, orientation='vertical', pad=0.02)
     cbar.set_label('Absolute Error', rotation=270, labelpad=15)
+    cbar.set_ticks([np.min(color_matrix), np.max(color_matrix)])
+    cbar.set_ticklabels(['{:.3f}'.format(np.min(color_matrix)), '{:.3f}'.format(np.max(color_matrix))])
 
     # Add label above the matrix (centered with the matrix)
     plt.text(cols / 2, rows + 0.5, 'Predicted Values', ha='center', va='center', fontsize=14)
@@ -62,16 +74,23 @@ def draw_grid(matrix, color_matrix, filename='output_plot.pdf'):  # Set the defa
     # plt.show()
 
 def main():
+    ref_abs_error = None
+    base_path = "./train-results/predicted-data-vis/"
+
     data_files = ["burn_6.csv", "burn_11.csv", "burn_16.csv", "burn_21.csv"]
     for file in data_files:
-        df = pd.read_csv(file)
+        df = pd.read_csv(base_path + file)
         df['absolute_error'] = abs(df['Actual'] - df['Predicted'])
 
         actual = np.array(df['Actual']).reshape(6, 6)
         predicted = np.array(df['Predicted']).reshape(6, 6)
         abs_error = np.array(df['absolute_error']).reshape(6, 6)
 
-        draw_grid(predicted, abs_error, filename=f'{file.replace(".csv", "")}.pdf')
+        # set the color scale to have the same range for all plots
+        if file == "burn_6.csv":
+            color_matrix = abs_error
+
+        draw_grid(predicted, abs_error, color_matrix, filename=f'{base_path}{file.replace(".csv", "")}.pdf')
 
 if __name__ == '__main__':
     main()
